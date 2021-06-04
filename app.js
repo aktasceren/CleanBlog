314eaf18a7679bfeb5885ca3463465c295a0437e
iamceren <aktasceren@outlook.com>
2021-06-04 18:09:34 +0300

    Add static files and create views

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
+const ejs = require('ejs');
+
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middleware
+app.use(express.static("public"));
+
 
 app.get('/', (req, res) => {
-  blog = { id: 1, 
-    title: "Blog title", 
-    description: "Blog description" }
-  res.send(blog)
-})
+  res.render('index');
+});
+
+app.get("/about", (req, res) => {
+  res.render('about');
+});
+
+app.get("/add_post", (req, res) => {
+  res.render('add_post');
+});
+
+app.get("/post", (req, res) => {
+  res.render('post');
+});
 
 const port = 3000;
 app.listen(port, () => {
